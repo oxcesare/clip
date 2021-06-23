65200a156244318cc451fa2e65580040b33d0f54
cesaralducinruiz <cesaralducinruiz@192.168.100.2>
2021-06-23 12:20:54 -0500

    Commit initial

---
+++ src/main/java/mx/com/examen/clip/ClipApplication.java
@@ -0,0 +1,13 @@
+package mx.com.examen.clip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/examen/clip/ClipApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.examen.clip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
